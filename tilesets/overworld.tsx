350ed23ad8d51dd8050d5e285e88c8c909dabc37
Connie Hilarides <conni_h@outlook.com>
2017-09-02 15:27:41 -0700

    MOAR SCIONS

--- tilesets/overworld.tsx
+++ tilesets/overworld.tsx
@@ -34,6 +34,48 @@
    <object id="1" x="1" y="0" width="14" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 16.0234,-15.9922 15.9922,0.0234375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="2" x="16" y="16.0313">
+    <polygon points="0,0 -16.0313,-15.9688 -16.0313,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0.03125" y="16.0313">
+    <polygon points="0,0 16.0313,-8 16.0313,0.03125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="0.15625" y="7.90625">
+    <polygon points="-0.171875,0.109375 15.8281,-7.96875 15.8438,8.0625 -0.171875,8.09375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.03125">
+    <polygon points="0,0 16.0313,8 16,16.0625 -0.046875,16.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="7.98438">
+    <polygon points="0,0 16.0313,7.98438 -0.0625,8.09375"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="16">
   <objectgroup draworder="index">
    <object id="2" x="1" y="0" width="15" height="16"/>
@@ -57,6 +99,51 @@
  <tile id="20" terrain="0,,0,0">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.03125" y="0">
+    <polygon points="0,0 15.9688,15.9688 15.9688,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index">
+   <object id="1" x="0.03125" y="15.9063">
+    <polygon points="0,0 16.0313,-15.9063 -0.0625,-15.9063"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.03125">
+    <polygon points="0,0 16.0938,-0.03125 16.0781,7.95313"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="7.98438">
+    <polygon points="0,0 16.0313,8.01563 16,-8.04688 0,-7.98438"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="15.9688">
+    <polygon points="0,0 16.0313,-8.01563 16.0625,-16.0938 -0.09375,-16.0313"/>
+   </object>
+   <object id="2" x="0" y="15.9688">
+    <polygon points="0,0 16.0313,-8.01563 16.0625,-16.0938 -0.09375,-16.0313"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index">
+   <object id="1" x="0.03125" y="7.98438">
+    <polygon points="0,0 15.9688,-8.01563 -0.09375,-8.01563"/>
+   </object>
   </objectgroup>
  </tile>
  <tile id="32" terrain=",0,,0">
@@ -84,6 +171,13 @@
    <object id="1" x="1" y="0" width="14" height="8"/>
   </objectgroup>
  </tile>
+ <tile id="37">
+  <objectgroup draworder="index">
+   <object id="4" x="1.03125" y="16">
+    <polygon points="0,0 1.875,-2 4.96875,-3 9,-3 12.0313,-2 13.9063,-0.03125"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="48" terrain=",0,,">
   <objectgroup draworder="index">
    <object id="1" x="1" y="0" width="15" height="16"/>
@@ -107,6 +201,56 @@
  <tile id="52">
   <objectgroup draworder="index">
    <object id="1" x="1" y="8" width="14" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="8" width="15" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8" width="15" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup draworder="index">
+   <object id="1" x="1" y="0" width="15" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15" height="8"/>
   </objectgroup>
  </tile>
 </tileset>
